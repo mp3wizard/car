289b5b9cc31424671e32859f844dd1d87414505d
Sappawish Siripon <mp3wizard@gmail.com>
2012-10-30 15:52:23 +0700

    งานสัปดาห์หนังสือ

---
+++ about.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+<html class="about">
+<head>
+	<meta charset="utf-8">
+	<title>PHILANTHROPY มนุษยธรรม</title>
+	<link rel="stylesheet" href="css/bootstrap.css" media="all" />
+	<link rel="stylesheet" href="css/bootstrap-responsive.css" media="all" />
+	<link rel="stylesheet" href="css/style2.css" media="all" />
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+	<!--[if (gte IE 6)&(lte IE 8)]>
+		<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6/html5shiv.min.js"></script>
+	  <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script>
+	<![endif]-->
+</head>
+<body class="">
+	<div id="container">
+		<header>
+			<h1><a href="http://www.honda.co.th" target="_blank">Honda</a></h1>
+			<h2>The Power of Dreams</h2>
+			<nav>
+				<ul>
+					<li><a href="http://www.hondacsrthailand.com/index.php#environment" target="_blank">Environment สิ่งแวดล้อม</a></li>
+					<li><a href="http://www.hondacsrthailand.com/index.php#safety" target="_blank">Safety ความปลอดภัยบนถนน</a></li>
+					<li><a href="http://www.hondacsrthailand.com/index.php#education" target="_blank">Education การศึกษา</a></li>
+					<li><a href="http://www.hondacsrthailand.com/index.php#philanthropy" target="_blank" class="active">Philanthropy มนุษยธรรม</a></li>
+				</ul>
+			</nav>
+		</header>
+		<div id="content">
+			<h2><img src="img/logo_hand.png" alt="" /></h2>
+			<nav>
+				<ul>
+					<li><a href="index.php">หน้าหลัก</a></li>
+					<li><a href="javascript:void(0);">เกี่ยวกับเรา</a></li>
+					<li><a href="javascript:void(0);">ภาพกิจกรรม</a></li>
+					<li><a href="javascript:void(0);">หน่วยงานที่เกี่ยวข้อง</a></li>
+					<li><a href="javascript:void(0);">สาระน่ารู้</a></li>
+					<li><a href="contact.php">ติดต่อเรา</a></li>
+				</ul>
+			</nav>
+			<article>
+
+			</article>
+		</div> <!-- #content -->
+		<footer>
+			<section id="emergency" class="clearfix">
+				<table id="call">
+					<caption>เบอร์โทรศัพท์ฉุกเฉิน 24 ชั่วโมง</caption>
+					<tbody>
+						<tr>
+							<td colspan="4" class="org"><a href="http://www.haii.or.th/" target="_blank">สถาบันสารสนเทศทรัพยากรน้ำและการเกษตร</a> </td>
+						</tr>
+                        <tr>
+							<td colspan="4" class="org"><a href="http://www.rtrc.in.th" target="_blank">สำนักงานบรรเทาทุกข์และประชานามัยพิทักษ์ สภากาชาดไทย</a></td>
+						</tr>
+						<tr>
+							<td class="org"><a href="http://www.emit.go.th/main/system/index.aspx" target="_blank">สถาบันการแพทย์ฉุกเฉินแห่งชาติ</a></td>
+							<td><a href="tel:1669">1669</a></td>
+							<td class="org"><a href="http://www.disaster.go.th/dpm/" target="_blank">กรมป้องกันและบรรเทาสาธารณภัย</a></td>
+							<td><a href="tel:1784">1784</a></td>
+						</tr>
+					</tbody>
+				</table>
+				<div id="subscribe">
+					<h3>ติดตามข่าวสารผ่านอีเมล์</h3>
+					<span class="inner"><label for="email" class="inner">Enter your E-mail address</label><input type="email" id="email" name="email" /><input type="submit" value="ลงทะเบียน" id="submit" /></span>
+				</div>
+			</section> <!-- #emergency -->
+			&copy; 2012 Honda Thailand Foundation. All Rights Reserved.
+		</footer>
+	</div>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.infieldlabel.min.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function($){ 
+			$('span.inner label').inFieldLabels();
+		});
+	</script>
+</body>
+</html>
